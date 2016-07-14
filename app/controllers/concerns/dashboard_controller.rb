6ef02e97a5eefc0b58fa134314b680216ac41152
Ahkeem Lang <langahkeem@gmail.com>
2016-07-14 09:00:41 -0500

    Cleaning up syntax errors

--- app/controllers/concerns/dashboard_controller.rb
+++ app/controllers/concerns/dashboard_controller.rb
@@ -1,5 +1,18 @@
 class DashboardController < ApplicationController
   def home
-    render template:'dashboard/index.html.erb'
+    render template: 'dashboard/index.html.erb', locals: {
+
+    }
+  end
+
+  def show
+    if Product.exists?(params[:id])
+      render template: 'dashboard/show.html.erb', locals: {
+        product: Product.find(params[:id])
+    elsif product.quantity = 0
+      render
+    else
+      render html: "Not Found", status: 404
+    end
   end
 end
